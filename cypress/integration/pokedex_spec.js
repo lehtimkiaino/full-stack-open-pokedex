1c1e34c64ec617d84f758f64e3bcfc337cfd3953
Aino Lehtimäki <lehtimki.aino@gmail.com>
2021-10-26 12:53:50 +0300

    tehtävä 11.9

--- cypress/integration/pokedex_spec.js
+++ cypress/integration/pokedex_spec.js
@@ -8,6 +8,7 @@ describe('Pokedex', function() {
   })
 
   it('poekmon page can be navigated to', function() {
+    cy.visit('http://localhost:5000')
     cy.visit('http://localhost:5000/pokemon/ivysaur')
     cy.contains('ivysaur')
     cy.contains('chlorophyll')
